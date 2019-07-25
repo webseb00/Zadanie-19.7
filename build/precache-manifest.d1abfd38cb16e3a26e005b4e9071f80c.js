e6ddd4fa2b694e52b4820c23f31282c8812b4e19
Sebastian Basiński <basinski.sebastian@gmail.com>
2019-07-25 11:58:30 +0200

    Production version

---
+++ build/precache-manifest.d1abfd38cb16e3a26e005b4e9071f80c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da244e713c628b65ba6755c792c76edd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "58876eb0eae3db5919b8",
+    "url": "/static/css/main.104bbe9b.chunk.css"
+  },
+  {
+    "revision": "8abcae5b4b8450cf8e9d",
+    "url": "/static/js/2.95f4aaaa.chunk.js"
+  },
+  {
+    "revision": "58876eb0eae3db5919b8",
+    "url": "/static/js/main.1256133e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
